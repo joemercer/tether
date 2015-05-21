719c4ef5c88d2fb102386faed061ea8fba1193de
Joe Mercer <mynameisjoemercer@gmail.com>
2015-05-21 18:41:22 -0400

    using a semantic-ui toolbar

--- src/js/components/Toolbar.jsx
+++ src/js/components/Toolbar.jsx
@@ -1,8 +1,5 @@
 const React = require('react');
 const ActionCreator = require('../actions/ActionCreators');
-const mui = require('material-ui');
-
-let {Toolbar, ToolbarGroup, DropDownMenu, FontIcon, DropDownIcon, RaisedButton} = mui;
 
 window.$ = window.jQuery = require('jquery');
 const Semantic = require('../../semantic/dist/semantic.min.js');
@@ -11,7 +8,7 @@ let Button = require('react-semantify').Button;
 
 
 
-let TetherToolbar = React.createClass({
+let Toolbar = React.createClass({
 
   triggerAddNewContact(e) {
     $('.ui.modal').modal('show');
@@ -19,16 +16,25 @@ let TetherToolbar = React.createClass({
 
   render() {
     return (
-      <Toolbar>
-        <ToolbarGroup key={0} float="left">
-          <span>Search Bar</span>
-        </ToolbarGroup>
-        <ToolbarGroup key={1} float="right">
-          <Button color="red" onClick={this.triggerAddNewContact}>Hello</Button>
-          <RaisedButton label="Add Contact" secondary={true} onClick={this.triggerAddNewContact} />
-          <span className="mui-toolbar-separator">&nbsp;</span>
-          <RaisedButton label="Login" primary={true} />
-        </ToolbarGroup>
+      <div class="ui menu">
+        <a class="active item">
+          <i class="home icon"></i> Home
+        </a>
+        <a class="item">
+          <i class="mail icon"></i> Messages
+        </a>
+        <Button color="red" onClick={this.triggerAddNewContact}>Hello</Button>
+        <Button color="blue" onClick={this.triggerAddNewContact}>Add Contact</Button>
+        <Button color="blue">Login</Button>
+        <div class="right menu">
+          <div class="item">
+            <div class="ui transparent icon input">
+              <input type="text" placeholder="Search..." />
+              <i class="search link icon"></i>
+            </div>
+          </div>
+        </div>
+
         <div className="ui modal">
           <i className="close icon"></i>
           <div className="header">
@@ -47,19 +53,11 @@ let TetherToolbar = React.createClass({
             <div className="ui button">OK</div>
           </div>
         </div>
-      </Toolbar>
 
-      // <div className="example-page">
-      //   <h1>Tether</h1>
-
-      //   <TaskList tasks={tasks} />
-
-      //   <RaisedButton label="Force Add Task" primary={true} onClick={this.handleAddNewClick} />
-      //   <RaisedButton label="Create Messages" secondary={true} onClick={this.handleClearListClick} />
-      // </div>
+      </div>
     );
   }
 
 });
 
-module.exports = TetherToolbar;
+module.exports = Toolbar;
