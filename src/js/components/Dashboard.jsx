bc56eb7057eaf44832109089bc98390022777e01
Joe Mercer <mynameisjoemercer@gmail.com>
2015-05-22 21:24:27 -0400

    refactored to messages view

--- src/js/components/Dashboard.jsx
+++ src/js/components/Dashboard.jsx
@@ -1,59 +1,17 @@
 const React = require('react');
 
-const MessageStore = require('../stores/MessageStore');
-const ContactStore = require('../stores/ContactStore');
-const ActionCreator = require('../actions/ActionCreators');
-
 const Messages = require('./Messages.jsx');
-
 
 let Dashboard = React.createClass({
 
   getInitialState() {
-    return {
-      tasks: []
-    }
-  },
-
-  _onChange() {
-    let self = this;
-
-    MessageStore.getFiltered(function(task){
-      return !task.sent;
-    }).then(function(tasks){
-      self.setState({
-        tasks: tasks
-      });
-    }).catch(function (err) {
-      console.log(err);
-    });
-  },
-
-  componentDidMount() {
-    let self = this;
-
-    MessageStore.addChangeListener(this._onChange);
-
-    MessageStore.getFiltered(function(task){
-      return !task.sent;
-    }).then(function(tasks){
-      self.setState({
-        tasks: tasks
-      });
-    }).catch(function (err) {
-      console.log(err);
-    });
-  },
-
-  componentWillUnmount() {
-    MessageStore.removeChangeListener(this._onChange);
+    return {}
   },
 
   render() {
-    let {tasks} = this.state;
     return (
       <div>
-        <Messages messages={tasks} />
+        <Messages />
       </div>
     );
   }
