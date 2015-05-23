ee587b30b672f8999fa6eff52ba0af134ff3679c
Joe Mercer <mynameisjoemercer@gmail.com>
2015-05-22 21:22:42 -0400

    pull messages data in from the Messages view

--- src/js/components/Messages.jsx
+++ src/js/components/Messages.jsx
@@ -1,15 +1,45 @@
 const React = require('react');
 const Task = require('./Message.jsx');
 
+const MessageStore = require('../stores/MessageStore');
+
+const GetMessages = function(){
+  return MessageStore.getFiltered(function(message){
+    return !message.sent;
+  });
+};
+
 let Messages = React.createClass({
-  getDefaultProps() {
+
+  getInitialState() {
     return {
       messages: []
-    };
+    }
+  },
+
+  _onChange() {
+    let self = this;
+
+    GetMessages().then(function(messages){
+      self.setState({
+        messages: messages
+      });
+    }).catch(function (err) {
+      console.log('!!! Error:' + err);
+    });
+  },
+
+  componentDidMount() {
+    MessageStore.addChangeListener(this._onChange);
+    this._onChange();
+  },
+
+  componentWillUnmount() {
+    MessageStore.removeChangeListener(this._onChange);
   },
 
   render() {
-    let {messages} = this.props;
+    let {messages} = this.state;
     return (
       <ul className="task-list">
         {messages.map(message =>
